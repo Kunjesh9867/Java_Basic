5087dc53b40e6faa1c2022a67e171d582f07f51f
Kunjesh Ramani <kunjeshramani9867@gmail.com>
2022-07-11 00:29:42 -0400

    File 12 added

---
+++ $12_ArrayForEach.java
@@ -0,0 +1,10 @@
+// ForEach loop for Array
+
+public class $12_ArrayForEach {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50};
+        for(int element:arr){
+            System.out.println(element);
+        }
+    }
+}
